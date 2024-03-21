87f5fd22008b70c129e69584343ba48a450e1e0b
Geethika-30 <112071177+Geethika-30@users.noreply.github.com>
2024-03-21 21:54:21 +0530

    Add files via upload

---
+++ Float Sub.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int mainl
+
+    float num1 = 5.8;
+    float num2 = 2.3;
+    float difference;
+    difference = num1 - num2;
+    printf("The difference between %f and %f is %f\n", num1, num2, difference);
+
+    return 0;
+}
